f6b13f497203a30382a6055916e530333be156c8
Dennis174698 <32445088+Dennis174698@users.noreply.github.com>
2019-10-25 14:41:06 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -29,7 +29,7 @@ mpirun  --hostfile host8 my_solution test_data/2048.in test_data/2048.out.8
 mpirun  --hostfile host16 my_solution test_data/2048.in test_data/2048.out.16
 mpirun  --hostfile host24 my_solution test_data/2048.in test_data/2048.out.24
 
-# second question
+# 
 mpirun  --hostfile host1 my_solution test_data/4096.in test_data/4096.out.1
 mpirun  --hostfile host2 my_solution test_data/4096.in test_data/4096.out.2
 mpirun  --hostfile host4 my_solution test_data/4096.in test_data/4096.out.4
