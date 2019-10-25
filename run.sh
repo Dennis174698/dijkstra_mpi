599e9aa3c3ffb1e2c14dbbba1aecabe5f6e19d13
Guangming Chen <21664707@ecm.uwa.edu.au>
2019-10-25 14:31:11 +0800

    v4

--- run.sh
+++ run.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
-
 gcc print_bin_result.c -o print_bin_result
 mpicc my_solution.c -o my_solution
 
-# test
-./my_solution test_data/4.in test_data/4.out
-./print_bin_result test_data/4.out 
+echo "n=1, host_num =1,input_data=4.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/4.in test_data/4.out
+echo "n=1,host_num=1,input_data=512.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/512.in test_data/512.out
 
-# 
-mpirun -n 1 my_solution test_data/2048.in test_data/2048.out.1
-mpirun -n 2 my_solution test_data/2048.in test_data/2048.out.2
-mpirun -n 4 my_solution test_data/2048.in test_data/2048.out.4
-mpirun -n 8 my_solution test_data/2048.in test_data/2048.out.8
-mpirun -n 16 my_solution test_data/2048.in test_data/2048.out.16
 
-#
-mpirun -n 1 my_solution test_data/256.in test_data/256.out.1
-mpirun -n 4 my_solution test_data/256.in test_data/256.out.4
-mpirun -n 1 my_solution test_data/512.in test_data/512.out.1
-mpirun -n 4 my_solution test_data/512.in test_data/512.out.4
+echo "n=1,host_num=1,input_data=2048.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/2048.in test_data/2048.out.1
+echo "n=2,host_num=2,input_data=2048.in"
+mpirun -n 2 --hostfile host2 my_solution test_data/2048.in test_data/2048.out.2
+echo "n=4,host_num=2,input_data=2048.in"
+mpirun -n 4 --hostfile host2 my_solution test_data/2048.in test_data/2048.out.4
+echo "n=8,host_num=2,input_data=2048.in"
+mpirun -n 8 --hostfile host2 my_solution test_data/2048.in test_data/2048.out.8
+echo "n=16,host_num=4,input_data=2048.in"
+mpirun -n 16 --hostfile host4 my_solution test_data/2048.in test_data/2048.out.16
 
-mpirun -n 1 my_solution test_data/1024.in test_data/1024.out.1
-mpirun -n 4 my_solution test_data/1024.in test_data/1024.out.4
 
-mpirun -n 1 my_solution test_data/2048.in test_data/2048.out.1
-mpirun -n 4 my_solution test_data/2048.in test_data/2048.out.4
+echo "n=1,host_num=1,input_data=512.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/512.in test_data/512.out.1
+echo "n=4,host_num=4,input_data=512.in"
+mpirun -n 4 --hostfile host4 my_solution test_data/512.in test_data/512.out.4
 
-mpirun -n 1 my_solution test_data/4096.in test_data/4096.out.1
-mpirun -n 4 my_solution test_data/4096.in test_data/4096.out.4
+echo "n=1,host_num=1,input_data=1024.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/1024.in test_data/1024.out.1
+echo "n=4,host_num=4,input_data=1024.in"
+mpirun -n 4 --hostfile host4 my_solution test_data/1024.in test_data/1024.out.4
+
+echo "n=1,host_num=1,input_data=2048.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/2048.in test_data/2048.out.1
+echo "n=4,host_num=4,input_data=2048.in"
+mpirun -n 4 --hostfile host4 my_solution test_data/2048.in test_data/2048.out.4
+
+echo "n=1,host_num=1,input_data=4096.in"
+mpirun -n 1 --hostfile host1 my_solution test_data/4096.in test_data/4096.out.1
+echo "n=4,host_num=4,input_data=4096.in"
+mpirun -n 4 --hostfile host4 my_solution test_data/4096.in test_data/4096.out.4
+
 
--- run2.sh
+++ run2.sh
@@ -1,30 +1,39 @@
-#PSB -l nodes=8:ppn=4
-source /ect/bash.bashrc
-
 
 gcc print_bin_result.c -o print_bin_result
 mpicc my_solution.c -o my_solution
 
 # test
-mpirun -n 12 --hostfile host1 my_solution test_data/4.in test_data/4.out
+mpirun  --hostfile host1 my_solution test_data/4.in test_data/4.out
 ./print_bin_result test_data/4.out 
 
 # numbers of compute nodes
-mpirun -n 12 --hostfile host1 my_solution test_data/2048.in test_data/2048.out.1
-mpirun -n 12 --hostfile host2 my_solution test_data/2048.in test_data/2048.out.2
-mpirun -n 12 --hostfile host3 my_solution test_data/2048.in test_data/2048.out.4
-mpirun -n 12 --hostfile host4 my_solution test_data/2048.in test_data/2048.out.8
-mpirun -n 12 --hostfile host5 my_solution test_data/2048.in test_data/2048.out.16
+mpirun  --hostfile host1 my_solution test_data/512.in test_data/512.out.1
+mpirun  --hostfile host2 my_solution test_data/512.in test_data/512.out.2
+mpirun  --hostfile host4 my_solution test_data/512.in test_data/512.out.4
+mpirun  --hostfile host8 my_solution test_data/512.in test_data/512.out.8
+mpirun  --hostfile host16 my_solution test_data/512.in test_data/512.out.16
+mpirun  --hostfile host24 my_solution test_data/512.in test_data/512.out.24
+#
+mpirun  --hostfile host1 my_solution test_data/1024.in test_data/1024.out.1
+mpirun  --hostfile host2 my_solution test_data/1024.in test_data/1024.out.2
+mpirun  --hostfile host4 my_solution test_data/1024.in test_data/1024.out.4
+mpirun  --hostfile host8 my_solution test_data/1024.in test_data/1024.out.8
+mpirun  --hostfile host16 my_solution test_data/1024.in test_data/1024.out.16
+mpirun  --hostfile host24 my_solution test_data/2048.in test_data/2048.out.24
+
+#
+mpirun  --hostfile host1 my_solution test_data/2048.in test_data/2048.out.1
+mpirun  --hostfile host2 my_solution test_data/2048.in test_data/2048.out.2
+mpirun  --hostfile host4 my_solution test_data/2048.in test_data/2048.out.4
+mpirun  --hostfile host8 my_solution test_data/2048.in test_data/2048.out.8
+mpirun  --hostfile host16 my_solution test_data/2048.in test_data/2048.out.16
+mpirun  --hostfile host24 my_solution test_data/2048.in test_data/2048.out.24
 
 # second question
-mpirun -n 12 --hostfile host1 my_solution test_data/512.in test_data/512.out.1
-mpirun -n 12 --hostfile host4 my_solution test_data/512.in test_data/512.out.4
+mpirun  --hostfile host1 my_solution test_data/4096.in test_data/4096.out.1
+mpirun  --hostfile host2 my_solution test_data/4096.in test_data/4096.out.2
+mpirun  --hostfile host4 my_solution test_data/4096.in test_data/4096.out.4
+mpirun  --hostfile host8 my_solution test_data/4096.in test_data/4096.out.8
+mpirun  --hostfile host16 my_solution test_data/4096.in test_data/4096.out.16
+mpirun  --hostfile host24 my_solution test_data/4096.in test_data/4096.out.24
 
-mpirun -n 12 --hostfile host1 my_solution test_data/1024.in test_data/1024.out.1
-mpirun -n 12 --hostfile host4 my_solution test_data/1024.in test_data/1024.out.4
-
-mpirun -n 12 --hostfile host1 my_solution test_data/2048.in test_data/2048.out.1
-mpirun -n 12 --hostfile host4 my_solution test_data/2048.in test_data/2048.out.4
-
-mpirun -n 12 --hostfile host1 my_solution test_data/4096.in test_data/4096.out.1
-mpirun -n 12 --hostfile host4 my_solution test_data/4096.in test_data/4096.out.4
